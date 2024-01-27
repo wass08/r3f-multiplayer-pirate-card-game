3a0138b246674b4dd8bb77ef64d40251b3ced8f3
wass08 <wass08@gmail.com>
2024-01-27 16:16:47 +0900

    package.json

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,13 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { isStreamScreen } from "playroomkit";
+import { degToRad } from "three/src/math/MathUtils";
 import { Gameboard } from "./Gameboard";
 import { MobileController } from "./MobileController";
 
 export const Experience = () => {
   return (
     <>
-      {isStreamScreen() && <OrbitControls />}
+      {isStreamScreen() && <OrbitControls maxPolarAngle={degToRad(80)} />}
       {isStreamScreen() ? <Gameboard /> : <MobileController />}
       <Environment preset="dawn" background blur={2} />
     </>
